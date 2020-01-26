973e72ffe734a554ca7e70b8f06b5c7a755c135a
Rushi Gandhi <rushi.gandhi@hotmail.com>
2020-01-25 23:05:45 -0500

    Directional Movement

--- client/seek.js
+++ client/seek.js
@@ -19,44 +19,57 @@ function plotCoords(list, colour) {
   }
 }
 
-function plotBasicIntersection() {
+function plotBasicIntersection(yPos, xPos) {
   list = [];
   separation = 10;
   for (var i = 0; i < 640; i += separation) {
     var x = i;
-    var y = 300;
-    list.push(createVector(x, y));
+    if (xPos) list.push(createVector(xPos, yPos));
+    else list.push(createVector(x, yPos));
   }
   return list;
 }
 
 function setupEntities() {
-//   food = randomCoords(50);
-  poison = randomCoords(10);
-  const x = plotBasicIntersection();
-  x.forEach(a => food.push(a));
+  food = randomCoords(0);
+  poison = randomCoords(0);
+
+  for (var j = 0; j < 100; j += 10) {
+    const z = plotBasicIntersection(j);
+    z.forEach(a => poison.push(a));
+  }
+
+  for (var j = 110; j < 290; j += 10) {
+    const z = plotBasicIntersection(j, 635);
+    z.forEach(a => food.push(a));
+  }
+
+  for (var j = 300; j < 640; j += 10) {
+    const z = plotBasicIntersection(j);
+    z.forEach(a => poison.push(a));
+  }
 }
 
 function setup() {
   createCanvas(640, 360);
   for (var i = 0; i < 10; i++) {
-    var x = random(width);
-    var y = random(height);
-    vehicles[i] = new Vehicle(x,y);
+    var y = random(115, 285);
+    var x = 0;
+    vehicles[i] = new Vehicle(x, y);
   }
   setupEntities();
 }
-function addNewFood(){
-    if (random(1) < 0.05) {
-        var x = random(width);
-        var y = random(height);
-        food.push(createVector(x, y));
-    }
+function addNewFood() {
+  if (random(1) < 0.5) {
+    var y = random(115, 285);
+    var x = random(0, 640);
+    food.push(createVector(x, y));
+  }
 }
 function draw() {
   background(0);
 
-//   addNewFood()
+  // addNewFood();
   let mouse = createVector(mouseX, mouseY);
 
   // Mouse
@@ -69,21 +82,19 @@ function draw() {
   plotCoords(poison, "red");
 
   // Steering
-  for (var i = vehicles.length-1; i >= 0; i--) {
-    vehicles[i].behaviours(food,poison);
+  for (var i = vehicles.length - 1; i >= 0; i--) {
+    vehicles[i].behaviours(food, poison);
     vehicles[i].boundaries();
     vehicles[i].update();
     vehicles[i].display();
-    var newVehicle = vehicles[i].reproduce()
-    if (newVehicle != null){
-        vehicles.push(newVehicle)
+    var newVehicle = vehicles[i].reproduce();
+    if (newVehicle != null) {
+      vehicles.push(newVehicle);
     }
     if (vehicles[i].retired()) {
-        var x = vehicles[i].position.x
-        var y = vehicles[i].position.y
-        food.push(createVector(x,y))
-        vehicles.splice(i,1)
+      var x = vehicles[i].position.x;
+      var y = vehicles[i].position.y;
+      vehicles.splice(i, 1);
     }
   }
-  
 }
--- client/vehicle.js
+++ client/vehicle.js
@@ -1,178 +1,183 @@
-var mr = 0.01
+var mr = 0.5;
 class Vehicle {
-    constructor(x, y, dna) {
-      this.acceleration = createVector(0, 0);
-      this.velocity = createVector(0, -2);
-      this.position = createVector(x, y);
-      this.r = 6;
-      this.maxspeed = 8;
-      this.maxforce = 0.2;
-      this.dna = []
-      if (dna === undefined) {
-        this.dna[0] = random(-2,2) // Food
-        this.dna[1] = random(-2,2) // Poison
-        this.dna[2] = random(0,100) // Food Perception
-        this.dna[3] = random(0,100) // Poison Perception
-        this.dna[4] = random(10,160) // Desired Seperation
-        this.dna[5] = random(0,8) // Seperation force
-      } else {
-          this.dna[0] = dna[0]
-          if (random(1) < mr) this.dna[0] += random(-0.1,0.1)
-          this.dna[1] = dna[1]
-          if (random(1) < mr) this.dna[1] += random(-0.1,0.1)
-          this.dna[2] = dna[2]
-          if (random(1) < mr) this.dna[2] += random(-10,10)
-          this.dna[3] = dna[3]
-          if (random(1) < mr) this.dna[3] += random(-10,10)
-          this.dna[4] = dna[4]
-          if (random(1) < mr) this.dna[4] += random(-8,8)
-          this.dna[5] = dna[5]
-          if (random(1) < mr) this.dna[5] += random(-1,1)
-      }
-      this.health = 1
+  constructor(x, y, dna) {
+    this.acceleration = createVector(0, 0);
+    this.velocity = createVector(0, -1);
+    this.position = createVector(x, y);
+    this.r = 6;
+    this.deathBy = 0;
+    this.maxspeed = 8;
+    this.maxforce = 0.2;
+    this.dna = [];
+    if (dna === undefined) {
+      this.dna[0] = random(-2, 2); // Food
+      this.dna[1] = -10; // Poison
+      this.dna[2] = random(0, 100); // Food Perception
+      this.dna[3] = random(0, 100); // Poison Perception
+      this.dna[4] = random(10, 160); // Desired Seperation
+      this.dna[5] = random(0, 8); // Seperation force
+    } else {
+      this.dna[0] = dna[0];
+      if (random(1) < mr) this.dna[0] += random(-0.1, 0.1);
+      this.dna[1] = dna[1];
+      if (random(1) < mr) this.dna[1] += random(-0.1, 0.1);
+      this.dna[2] = dna[2];
+      if (random(1) < mr) this.dna[2] += random(-10, 10);
+      this.dna[3] = dna[3];
+      if (random(1) < mr) this.dna[3] += random(-10, 10);
+      this.dna[4] = dna[4];
+      if (random(1) < mr) this.dna[4] += random(-8, 8);
+      this.dna[5] = dna[5];
+      if (random(1) < mr) this.dna[5] += random(-1, 1);
+    }
+    this.health = 1;
+  }
+
+  boundaries() {
+    var d = 25;
+
+    let desired = null;
+
+    if (this.position.x < d) {
+      desired = createVector(this.maxspeed, this.velocity.y);
+    } else if (this.position.x > width - d) {
+      desired = createVector(-this.maxspeed, this.velocity.y);
     }
 
-    boundaries() {
-        var d = 25;
-
-        let desired = null;
-    
-        if (this.position.x < d) {
-          desired = createVector(this.maxspeed, this.velocity.y);
-        } else if (this.position.x > width - d) {
-          desired = createVector(-this.maxspeed, this.velocity.y);
-        }
-    
-        if (this.position.y < d) {
-          desired = createVector(this.velocity.x, this.maxspeed);
-        } else if (this.position.y > height - d) {
-          desired = createVector(this.velocity.x, -this.maxspeed);
-        }
-    
-        if (desired !== null) {
-          desired.normalize();
-          desired.mult(this.maxspeed);
-          let steer = p5.Vector.sub(desired, this.velocity);
-          steer.limit(this.maxforce);
-          this.applyForce(steer);
-        }
-      }
-  
-    update() {
-      this.health -= 0.005;
-      this.velocity.add(this.acceleration);
-      this.velocity.limit(this.maxspeed);
-      this.position.add(this.velocity);
-      this.acceleration.mult(0);
-    }
-  
-    applyForce(force) {
-      this.acceleration.add(force);
+    if (this.position.y < d) {
+      desired = createVector(this.velocity.x, this.maxspeed);
+    } else if (this.position.y > height - d) {
+      desired = createVector(this.velocity.x, -this.maxspeed);
     }
 
-    behaviours(good, bad) {
-        let separateForce = this.separate(vehicles);
-        var foodSteer = this.eat(good, 0.2, this.dna[2]);
-        var poisonSteer = this.eat(bad, -0.5, this.dna[3]);
-        foodSteer.mult(this.dna[0])
-        poisonSteer.mult(this.dna[1])
-        separateForce.mult(5);
-        this.applyForce(foodSteer)
-        this.applyForce(separateForce);
-        this.applyForce(poisonSteer)
-    }
-
-    retired() {
-        return this.health < 0
-    }
-
-    reproduce(){
-        if (random(1)<0.001){
-            return new Vehicle(this.position.x+random(-0.1,0.1),this.position.y+random(-0.1,0.1),this.dna)
-        } 
-        return null
-    }
-
-    eat(list, nutrition, perception) {
-        var record = Infinity
-        var closest = null
-        for (var i = list.length-1; i >= 0; i--){
-            var d = this.position.dist(list[i])
-            if (d < this.maxspeed) {
-                list.splice(i, 1)
-                this.health += nutrition
-            } else if (d<record  && d < perception) {
-                record = d
-                closest = list[i]
-            }
-        }
-        if (closest != null){
-            return this.seek(closest);
-        }
-        return createVector(0,0)
-     }
-  
-    seek(target) {
-  
-      var desired = p5.Vector.sub(target, this.position);
-  
-      desired.setMag(this.maxspeed);
-  
-      var steer = p5.Vector.sub(desired, this.velocity);
+    if (desired !== null) {
+      desired.normalize();
+      desired.mult(this.maxspeed);
+      let steer = p5.Vector.sub(desired, this.velocity);
       steer.limit(this.maxforce);
-  
-      // this.applyForce(steer);
-      return steer
+      this.applyForce(steer);
     }
+  }
 
-    separate(vehicles) {
-        let desiredseparation = this.dna[4];
-        let sum = createVector();
-        let count = 0;
-        // For every boid in the system, check if it's too close
-        for (let i = 0; i < vehicles.length; i++) {
-          let d = p5.Vector.dist(this.position, vehicles[i].position);
-          // If the distance is greater than 0 and less than an arbitrary amount (0 when you are yourself)
-          if ((d > 0) && (d < desiredseparation)) {
-            // Calculate vector pointing away from neighbor
-            let diff = p5.Vector.sub(this.position, vehicles[i].position);
-            diff.normalize();
-            diff.div(d); // Weight by distance
-            sum.add(diff);
-            count++; // Keep track of how many
-          }
-        }
-        // Average -- divide by how many
-        if (count > 0) {
-          sum.div(count);
-          // Our desired vector is the average scaled to maximum speed
-          sum.normalize();
-          sum.mult(this.maxspeed);
-          // Implement Reynolds: Steering = Desired - Velocity
-          sum.sub(this.velocity);
-          sum.limit(this.maxforce);
-        }
-        return sum;
+  update() {
+    this.health -= 0.005;
+    this.velocity.add(this.acceleration);
+    this.velocity.limit(this.maxspeed);
+    this.position.add(this.velocity);
+    this.acceleration.mult(0);
+  }
+
+  applyForce(force) {
+    this.acceleration.add(force);
+  }
+
+  behaviours(good, bad) {
+    let separateForce = this.separate(vehicles);
+    var foodSteer = this.eat(good, 0.2, this.dna[2]);
+    var poisonSteer = this.eat(bad, -5, this.dna[3]);
+    foodSteer.mult(this.dna[0]);
+    poisonSteer.mult(this.dna[1]);
+    separateForce.mult(5);
+    this.applyForce(foodSteer);
+    this.applyForce(separateForce);
+    this.applyForce(poisonSteer);
+  }
+
+  retired() {
+    if (this.health < 0) {
+      return true;
+    } else if (this.position.x >= 640 || this.position.y >= 360) {
+      this.deathBy = 1;
+      return true;
+    }
+    return false;
+  }
+
+  reproduce() {
+    if (this.deathBy) {
+      return new Vehicle(0, random(110, 290), this.dna);
+    }
+    return null;
+  }
+
+  eat(list, nutrition, perception) {
+    var record = Infinity;
+    var closest = null;
+    for (var i = list.length - 1; i >= 0; i--) {
+      var d = this.position.dist(list[i]);
+      if (d < this.maxspeed) {
+        this.health += nutrition;
+      } else if (d < record && d < perception) {
+        record = d;
+        closest = list[i];
       }
-  
-    display() {
-      let theta = this.velocity.heading() + PI / 2;
-      
-      push();
-      translate(this.position.x, this.position.y);
-      rotate(theta);
-    // Direction vector
-    stroke(0,255,0);
-    noFill();
-    line(0,0,0,-this.dna[0]*20);
-    ellipse(0,0,-this.dna[2]*2);
-    stroke(255,0,0);
-    line(0,0,0,this.dna[1]*20);
-    ellipse(0,0,this.dna[3]*2);
+    }
+    if (closest != null) {
+      return this.seek(closest);
+    }
+    return createVector(0, 0);
+  }
 
-    var green = color(0,255,0)
-    var red = color(255,0,0)
-    var carColor = lerpColor(red, green, this.health)
+  seek(target) {
+    var desired = p5.Vector.sub(target, this.position);
+
+    desired.setMag(this.maxspeed);
+
+    var steer = p5.Vector.sub(desired, this.velocity);
+    steer.limit(this.maxforce);
+
+    // this.applyForce(steer);
+    return steer;
+  }
+
+  separate(vehicles) {
+    let desiredseparation = this.dna[4];
+    let sum = createVector();
+    let count = 0;
+    // For every boid in the system, check if it's too close
+    for (let i = 0; i < vehicles.length; i++) {
+      let d = p5.Vector.dist(this.position, vehicles[i].position);
+      // If the distance is greater than 0 and less than an arbitrary amount (0 when you are yourself)
+      if (d > 0 && d < desiredseparation) {
+        // Calculate vector pointing away from neighbor
+        let diff = p5.Vector.sub(this.position, vehicles[i].position);
+        diff.normalize();
+        diff.div(d); // Weight by distance
+        sum.add(diff);
+        count++; // Keep track of how many
+      }
+    }
+    // Average -- divide by how many
+    if (count > 0) {
+      sum.div(count);
+      // Our desired vector is the average scaled to maximum speed
+      sum.normalize();
+      sum.mult(this.maxspeed);
+      // Implement Reynolds: Steering = Desired - Velocity
+      sum.sub(this.velocity);
+      sum.limit(this.maxforce);
+    }
+    return sum;
+  }
+
+  display() {
+    let theta = this.velocity.heading() + PI / 2;
+
+    push();
+    translate(this.position.x, this.position.y);
+    rotate(theta);
+    // Direction vector
+    stroke(0, 255, 0);
+    noFill();
+    line(0, 0, 0, -this.dna[0] * 20);
+    ellipse(0, 0, -this.dna[2] * 2);
+    stroke(255, 0, 0);
+    line(0, 0, 0, this.dna[1] * 20);
+    ellipse(0, 0, this.dna[3] * 2);
+
+    var green = color(0, 255, 0);
+    var red = color(255, 0, 0);
+    var carColor = lerpColor(red, green, this.health);
 
     fill(carColor);
     stroke(carColor);
@@ -184,5 +189,5 @@ class Vehicle {
     endShape(CLOSE);
 
     pop();
-    }
   }
+}
